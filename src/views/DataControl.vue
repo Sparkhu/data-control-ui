aec9c34cb2a18b869254fb96f38867179fc4123f
seoh02h <seok02h@khu.ac.kr>
2022-05-13 23:08:31 +0900

    Sidebar 및 데이터 관리 화면 추가

---
+++ src/views/DataControl.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrapper">
+    <div class="table-header">데이터관리</div>
+    <div class="table-main">
+      <div class="table-main__header">HDFS 데이터 관리 및 분석 설정</div>
+      <div>
+        <div class="table-main__content">
+          <div class="table-main__content-intro">
+            분석을 위한 보존 기간 및 분석 기간을 설정합니다.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.wrapper {
+  margin: 10px;
+}
+.table-header {
+  color: #727e8c;
+  font-size: 25px;
+  margin: 20px 30px;
+}
+.table-main__content-intro {
+  color: #8a99b5;
+  margin-left: 25px;
+}
+</style>
